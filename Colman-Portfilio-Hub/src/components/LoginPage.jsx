2df6ea4cb24c8636eded78b06f554b35d3907ea2
Bar Rachmany <rachmanybar@gmail.com>
2024-07-24 16:35:06 +0300

    create carousel

--- Colman-Portfilio-Hub/src/components/LoginPage.jsx
+++ Colman-Portfilio-Hub/src/components/LoginPage.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import MainPage from './MainPage'
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
     return (
         <div>
             <h1>Welcome To Colman Portfilio Hub</h1>
             <input type="text" placeholder="Enter your ID" />
-            <Link to="/MainPage"><button>Connect</button></Link>
+            <Link to="/main"><button>Connect</button></Link>
         </div>
     );
 };
--- Colman-Portfilio-Hub/src/components/MainPage.jsx
+++ Colman-Portfilio-Hub/src/components/MainPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import PhotoCarousel from './PhotoCarousel';
 
 const MainPage = () => {
     return (
         <div>
             <h1>Colman Portfilio Hub</h1>
-
+            <PhotoCarousel />
         </div>
     );
 };
