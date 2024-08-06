3e0035dfb117fee0d304dfd0500ab7ebea4a72d4
OmerYaakov <omeryaakov2@gmail.com>
2024-08-06 13:01:46 +0300

    updated the register func

--- Colman-Portfilio-Hub/src/components/RegisterPage.jsx
+++ Colman-Portfilio-Hub/src/components/RegisterPage.jsx
@@ -1,62 +1,89 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const RegisterPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [newUser, setNewUser] = useState({
-        name: "",
-        email: "",
-        id: "",
-        password: "",
-        confirmPassword: ""
-    });
+  const [newUser, setNewUser] = useState({
+    name: "",
+    email: "",
+    id: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-    const handleChange = (e) => {
-        setNewUser({ ...newUser, [e.target.name]: e.target.value });
-        console.log(newUser);
-    };
+  const handleChange = (e) => {
+    setNewUser({ ...newUser, [e.target.name]: e.target.value });
+    console.log(newUser);
+  };
 
-    const validateInputs = () => {
-        if (newUser.password == "" || newUser.email == "" || newUser.id == "" || newUser.name == "") {
-            alert("Please fill all the fields");
-            return false;
-        }
+  const validateInputs = () => {
+    if (newUser.password == "" || newUser.email == "" || newUser.id == "" || newUser.name == "") {
+      alert("Please fill all the fields");
+      return false;
     }
+  };
 
-    const handleRegister = async () => {
-        if (!validateInputs()) {
-            return;
-        }
-        axios.post("http://localhost:5000/user/register", newUser)
-            .then((response) => {
-                console.log(response);
-                // remove the old tokens
-                localStorage.removeItem("accessToken");
-                localStorage.removeItem("refreshToken");
-                // set the new tokens
-                localStorage.setItem("accessToken", response.data.accessToken);
-                localStorage.setItem("refreshToken", response.data.refreshToken);
-                navigate("/main");
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+  const handleRegister = async () => {
+    if (validateInputs()) {
+      return;
     }
+    axios
+      .post("http://localhost:5000/user/register", newUser)
+      .then((response) => {
+        console.log(response);
+        // remove the old tokens
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        // set the new tokens
+        localStorage.setItem("accessToken", response.data.accessToken);
+        localStorage.setItem("refreshToken", response.data.refreshToken);
+        navigate("/main");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
-        <div className="login-container">
-            <div className="login-inner-container">
-                <h2 className="h2-login">Register</h2>
-                <input name='name' type="text" placeholder="Name" className="login-input" onChange={handleChange} />
-                <input name='email' type="email" placeholder="Email" className="login-input" onChange={handleChange} />
-                <input name='id' type="number" placeholder="ID" className="login-input" onChange={handleChange} />
-                <input name='password' type="password" placeholder="Password" className="login-input" onChange={handleChange} />
-                <button className="button-login" onClick={handleRegister}>Register</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="login-container">
+      <div className="login-inner-container">
+        <h2 className="h2-login">Register</h2>
+        <input
+          name="name"
+          type="text"
+          placeholder="Name"
+          className="login-input"
+          onChange={handleChange}
+        />
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          className="login-input"
+          onChange={handleChange}
+        />
+        <input
+          name="id"
+          type="number"
+          placeholder="ID"
+          className="login-input"
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          className="login-input"
+          onChange={handleChange}
+        />
+        <button className="button-login" onClick={handleRegister}>
+          Register
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
