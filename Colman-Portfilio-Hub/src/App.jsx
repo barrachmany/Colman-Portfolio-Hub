2c4abd830bd1d2fcbcd27d305a57d31420ac5cc4
OmerYaakov <omeryaakov2@gmail.com>
2024-08-12 19:39:15 +0300

    updated the my profile route

--- Colman-Portfilio-Hub/src/App.jsx
+++ Colman-Portfilio-Hub/src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './components/MainPage.jsx'
-import LandingPage from './components/LandingPage.jsx';
-import LoginPage from './components/LoginPage.jsx';
-import RegisterPage from './components/RegisterPage.jsx';
-import CreateProjectPage from './components/CreateProjectPage.jsx';
-import PhotoCarousel from './components/PhotoCarousel.jsx';
-import AppContext from './AppContext.jsx';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./components/MainPage.jsx";
+import LandingPage from "./components/LandingPage.jsx";
+import LoginPage from "./components/LoginPage.jsx";
+import RegisterPage from "./components/RegisterPage.jsx";
+import CreateProjectPage from "./components/CreateProjectPage.jsx";
+import PhotoCarousel from "./components/PhotoCarousel.jsx";
+import AppContext from "./AppContext.jsx";
+import ProfilePage from "./components/ProfilePage.jsx";
 
 function App() {
-
   const [projects, setProjects] = useState([]);
-  const [user, setUser] = useState({});  
+  const [user, setUser] = useState({});
 
   return (
     <>
       <AppContext.Provider value={{ projects, setProjects, user, setUser }}>
-        <div className='app'>
+        <div className="app">
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<LandingPage />} />
@@ -25,10 +25,12 @@ function App() {
               <Route path="/register" element={<RegisterPage />} />
               <Route path="/main" element={<MainPage />} />
               <Route path="/createproject" element={<CreateProjectPage />} />
+              <Route path="/myProfile" element={<ProfilePage />} />
             </Routes>
           </BrowserRouter>
         </div>
       </AppContext.Provider>
+    </>
   );
 }
 
