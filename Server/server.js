9d20622743c0f04316fbac7c453d271f3f7dceca
eilonALT <eilon159123@gmail.com>
2024-08-31 15:10:03 +0300

    expose server

--- Server/server.js
+++ Server/server.js
@@ -4,7 +4,7 @@ import http from 'http';
 initApp().then((app) => {
   if (process.env.NODE_ENV !== 'production') {
     console.log('DEVELOPMENT');
-    http.createServer(app).listen(process.env.DEV_PORT);
+    http.createServer(app).listen(process.env.DEV_PORT,"0.0.0.0");
     console.log(`Server is running on port: ${process.env.DEV_PORT}`);
   } else {
     console.log('PRODUCTION');
